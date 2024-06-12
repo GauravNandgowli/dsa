3256fbf8313fa7deb594c8b837f86d5ad3a2894f
GauravNandgowli <me.nandgowligaurav@gmail.com>
2024-06-12 16:55:52 +0530

    initial commit

---
+++ InsertionSort.java
@@ -0,0 +1,22 @@
+class insertionSort {
+
+    public static void main(String[] args) {
+        int arr[] = { 13, 46, 24, 52, 20, 9 };
+        for (int i = 0; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+
+        System.out.println("After insertion sort: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
---
+++ SelectionSort.java
@@ -0,0 +1,24 @@
+class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = { 13, 46, 24, 52, 20, 9 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+
+        }
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
